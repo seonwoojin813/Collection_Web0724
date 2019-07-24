f33bf07fb975cdd9b9183f57dfa5ee6490b4a23e
seonwoojin813 <47738049+seonwoojin813@users.noreply.github.com>
2019-07-24 10:23:27 +0900

    Initial Commit

---
+++ Collection_Web0724/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection_Web0724
+//
+//  Created by Woojin on 24/07/2019.
+//  Copyright © 2019 Woojin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
